4079f4ca1393236210a6c8eda0b261f08700baea
elizavetarybina <liza.rybina@inbox.ru>
2025-07-07 16:47:05 +0300

    refactor

--- frontend/src/app/routes/AppRoutes.tsx
+++ frontend/src/app/routes/AppRoutes.tsx
@@ -2,6 +2,7 @@ import { ScrollToTop } from '@/components/common/ScrollToTop'
 import AdminLayout from '@/layout/AdminLayout'
 import AppLayout from '@/layout/AppLayout'
 import { CoursePage, HomePage, NotFound } from '@/pages'
+import PersonalAccountStudent from '@/pages/Account/PersonalAccountStudent'
 import CourseCreatePage from '@/pages/Admin/CourseCreatePage'
 import StudentUsers from '@/pages/Admin/StudentUsers'
 import TeacherUsers from '@/pages/Admin/TeacherUsers'
@@ -13,74 +14,77 @@ import GDevelopEditor from '@/pages/GDevelop/GDevelopEditor'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 const router = createBrowserRouter([
-  {
-    element: (
-      <>
-        <ScrollToTop />
-        <AppLayout />
-      </>
-    ),
-    children: [
-      {
-        path: '/',
-        element: <HomePage />,
-      },
-      {
-        path: '/course',
-        element: <CoursePage />,
-      },
-      {
-        path: '/course/physiology',
-        element: <CourseInnerPage />
-      },
-    ]
-  },
-  {
-    element: (
-      <>
-        <ScrollToTop />
-        <AdminLayout />
-      </>
-    ),
-    children: [
-      {
-        path: '/admin/dashboard',
-        element: <HomeAdmin />,
-      },
-      {
-        path: '/admin/teachers',
-        element: <TeacherUsers />,
-      },
-      {
-        path: '/admin/students',
-        element: <StudentUsers />,
-      },
-      {
-        path: '/admin/courses/new',
-        element: <CourseCreatePage />
-      }
-      
-    ]
-  },
-  {
-    path: '/signin',
-    element: <SignIn />
-  },
-  {
-    path: '/games',
-    element: <GDevelopEditor />
-  },
-  {
-    path: '/signup',
-    element: <SignUp />
-  },
-  // Fallback route
-  {
-    path: '*',
-    element: <NotFound />,
-  },
-]);
+	{
+		element: (
+			<>
+				<ScrollToTop />
+				<AppLayout />
+			</>
+		),
+		children: [
+			{
+				path: '/',
+				element: <HomePage />
+			},
+			{
+				path: '/course',
+				element: <CoursePage />
+			},
+			{
+				path: '/course/physiology',
+				element: <CourseInnerPage />
+			},
+			{
+				path: '/profile',
+				element: <PersonalAccountStudent />
+			}
+		]
+	},
+	{
+		element: (
+			<>
+				<ScrollToTop />
+				<AdminLayout />
+			</>
+		),
+		children: [
+			{
+				path: '/admin/dashboard',
+				element: <HomeAdmin />
+			},
+			{
+				path: '/admin/teachers',
+				element: <TeacherUsers />
+			},
+			{
+				path: '/admin/students',
+				element: <StudentUsers />
+			},
+			{
+				path: '/admin/courses/new',
+				element: <CourseCreatePage />
+			}
+		]
+	},
+	{
+		path: '/signin',
+		element: <SignIn />
+	},
+	{
+		path: '/games',
+		element: <GDevelopEditor />
+	},
+	{
+		path: '/signup',
+		element: <SignUp />
+	},
+	// Fallback route
+	{
+		path: '*',
+		element: <NotFound />
+	}
+])
 
 export const AppRouter = () => {
-  return <RouterProvider router={router} />;
+	return <RouterProvider router={router} />
 }
