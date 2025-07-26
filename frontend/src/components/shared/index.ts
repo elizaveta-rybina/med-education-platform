ccb0b320f14a9a043b043cebc5ffe1c999207d6a
elizavetarybina <liza.rybina@inbox.ru>
2025-07-26 19:45:45 +0300

    #fix: path fix

--- frontend/src/components/shared/index.ts
+++ frontend/src/components/shared/index.ts
@@ -1,6 +1,5 @@
 import { ModalButtonAuthentication, NavButton } from './buttons'
-import Header from './header'
 import ModalSignIn from './modalSignIn'
 import Subtitle from './subtitle'
 
-export { Header, ModalButtonAuthentication, ModalSignIn, NavButton, Subtitle }
+export { ModalButtonAuthentication, ModalSignIn, NavButton, Subtitle }
